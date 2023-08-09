d37f2a51f6f88c0fe2d430fceabee7c57f9e00f1
Joabesv <joabevarjao123@gmail.com>
2023-08-08 23:51:15 -0300

    try vercel deploy

--- app/src/modules/events/handlers/events.ts
+++ app/src/modules/events/handlers/events.ts
@@ -5,5 +5,5 @@ export const getEventsHandler: RouteHandler = async (request, reply) => {
   return reply.send({
     name: 'Joabe',
     age: 17
-  })  
+   })  
 }
--- app/src/server.ts
+++ app/src/server.ts
@@ -1,15 +1,16 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { buildApp } from './app';
 import { logger } from './config/logger';
 
-async function start() {
+
+export default async (request: FastifyRequest, reply: FastifyReply) => {
   const app = await buildApp({ logger });
   try {
     logger.info(app.printRoutes());
-    app.listen({ port: 5000, host: '0.0.0.0' })
+    app.ready();
+    app.server.emit('request', request, reply)
   } catch(error) {
     app.log.fatal({ error },'error on startup')
     throw error;
   }
 }
-
-start();
