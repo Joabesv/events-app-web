fccb7aead627dd0d11c7bb89b271f33c9e2e44ee
beatriz.santos <beatriz.santos@prima.com.br>
2023-08-09 15:44:35 -0300

    create forms component and page

--- src/components/CalendarForm.vue
+++ src/components/CalendarForm.vue
@@ -1,63 +1,76 @@
 <script setup lang="ts">
-    import {ref,watch} from 'vue'
-    import {Close} from '@element-plus/icons-vue'
+import { ref, watch } from "vue";
+import { Close } from "@element-plus/icons-vue";
 
-    const endDateVisible = ref(false)
-    const initialDate = ref('')
-    const endDate = ref('')
+const endDateVisible = ref(false);
+const initialDate = ref("");
+const endDate = ref("");
 
-    const emit = defineEmits(['newInit','newEnd'])
+const emit = defineEmits(["newInit", "newEnd"]);
 
-    watch(initialDate,(newInitialDate)=>{
-        emit('newInit',newInitialDate)
-    })
+watch(initialDate, (newInitialDate) => {
+  emit("newInit", newInitialDate);
+});
 
-    watch(endDate,(newEndDate)=>{
-        emit('newEnd',newEndDate)
-    })
+watch(endDate, (newEndDate) => {
+  emit("newEnd", newEndDate);
+});
 
-    function closeCalendar(){
-        endDateVisible.value=false
-        endDate.value = ''
-    }
-
+function closeCalendar() {
+  endDateVisible.value = false;
+  endDate.value = "";
+}
 </script>
 <template>
-    <div class="calendarLayout">
-        <div>
-            <label class="formLabel">Data de início</label>
-            <s-date-picker v-model="initialDate" :style="{widht:'45%'}"/>    
-        </div>
-        <s-button v-if="!endDateVisible.valueOf()" label="+ data final" class="add-btn" @click="()=>{endDateVisible=true}"/>
+  <div class="calendarLayout">
+    <div>
+      <label class="formLabel">Data de início</label>
+      <s-date-picker v-model="initialDate" :style="{ widht: '45%' }" />
     </div>
-    <div class="calendarLayout" v-if="endDateVisible.valueOf()">
-        <div>
-            <label class="formLabel">Data final</label>
-            <s-date-picker v-model="endDate" :style="{widht:'45%'}"/>    
-        </div>
-        <el-button id="cancel" @click="closeCalendar"><el-icon><Close /></el-icon></el-button>
+    <s-button
+      v-if="!endDateVisible.valueOf()"
+      label="+ data final"
+      class="add-btn"
+      @click="
+        () => {
+          endDateVisible = true;
+        }
+      "
+    />
+  </div>
+  <div class="calendarLayout" v-if="endDateVisible.valueOf()">
+    <div>
+      <label class="formLabel">Data final</label>
+      <s-date-picker v-model="endDate" :style="{ widht: '45%' }" />
     </div>
+    <el-button id="cancel" @click="closeCalendar"
+      ><el-icon><Close /></el-icon
+    ></el-button>
+  </div>
 </template>
 <style scoped>
+.formLabel {
+  font-weight: bold;
+  font-size: 16px;
+}
+.calendarLayout {
+  display: grid;
+  grid-template-columns: 1.75fr 0.25fr;
+  align-items: center;
+  gap: 10px;
+}
 
-    .calendarLayout{
-        display: grid;
-        grid-template-columns: 1.75fr 0.25fr;
-        align-items: center;
-        gap: 10px;
-    }
+#cancel {
+  background-color: white;
+  border: 1px solid #98a1a8;
+  color: #98a1a8;
+  width: 40px;
+  margin-top: 4px;
+}
 
-    #cancel{
-        background-color: white;
-        border: 1px solid #98A1A8;
-        color: #98A1A8;
-        width: 40px;
-    }
-
-    .add-btn {
-        background-color: #F0F0F0;
-        color: #98A1A8;
-        border: none;
-    }
-
+.add-btn {
+  background-color: #f0f0f0;
+  color: #98a1a8;
+  border: none;
+}
 </style>
--- src/views/Event/viewCreate.vue
+++ src/views/Event/viewCreate.vue
@@ -1,62 +1,145 @@
 <script setup lang="ts">
-    import {reactive} from 'vue'
-    import CalendarForm from '../../components/CalendarForm.vue'
-    import HorarioForm from '../../components/HorarioForm.vue'
-    import {Location,Link} from '@element-plus/icons-vue'
-    let form = reactive({})
+import { reactive } from "vue";
+import CalendarForm from "../../components/CalendarForm.vue";
+import HorarioForm from "../../components/HorarioForm.vue";
+import PriceForm from "../../components/PriceForm.vue";
+import LimitForm from "../../components/LimitForm.vue";
+import { Location, Link } from "@element-plus/icons-vue";
+let form = reactive({});
 </script>
 <template>
-    <s-nav-bar titulo="Criar evento" :voltar="()=>{$router.push('/')}"/>
-    <s-container>
-        <s-card :style="{marginBottom:'16px'}">
-            <div>
-                <label class="formLabel">Título</label>
-                <s-input v-model="form.nome" size="large" placeholder="Escreva aqui :)" ></s-input>
-            </div>
-            <CalendarForm @newInit="(value)=>{form.initialDate=value}" @newEnd="(value)=>{form.endDate=value}"/>
-            <HorarioForm @newInit="(value)=>{form.initialHour=value}" @newEnd="(value)=>{form.endHour=value}"/>
-            <div>
-                <label class="formLabel">Local</label>
-                <s-input v-model="form.endereco" placeholder="Digite o endereço" :prefix-icon="Location" />
-            </div>
-            <div :style="{display:'flex',justifyContent:'space-between',alignItems:'center'}">
-                <label>Gerar comunicado com IA</label>
-                <s-switch v-model="form.ia" />
-            </div>
-            <el-input
-                v-model="form.texto"
-                :rows="3"
-                :style="{marginBottom:'22px'}"
-                type="textarea"
-                placeholder="Texto do comunicado"
-            />
-            <div>
-                <label>Enviar para</label>
-                <s-input v-model="form.destino" size="large" placeholder="" ></s-input>
-            </div>
-            <div>
-                <el-upload
-                    class="upload-demo"
-                    drag
-                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-                >
-                    <span>
-                        <el-icon><Link /></el-icon>
-                        Adicionar anexo
-                    </span>
-                    <template #tip>
-                        <div class="el-upload__tip">
-                            max file size: 1MB
-                        </div>
-                    </template>
-                </el-upload>
-            </div>
-        </s-card>
-        <s-button-menu label="Enviar" variant="primary-green" @click="()=>{console.log(form)}"/>
-    </s-container>
+  <s-nav-bar
+    titulo="Criar evento"
+    :voltar="
+      () => {
+        $router.push('/');
+      }
+    "
+  />
+  <div class="page-container">
+  <s-container>
+    <s-card :style="{ marginBottom: '16px' }">
+      <div>
+        <label class="formLabel">Título</label>
+        <s-input
+          v-model="form.nome"
+          size="large"
+          placeholder="Escreva aqui :)"
+        ></s-input>
+      </div>
+      <CalendarForm
+        @newInit="
+          (value) => {
+            form.initialDate = value;
+          }
+        "
+        @newEnd="
+          (value) => {
+            form.endDate = value;
+          }
+        "
+      />
+      <HorarioForm
+        @newInit="
+          (value) => {
+            form.initialHour = value;
+          }
+        "
+        @newEnd="
+          (value) => {
+            form.endHour = value;
+          }
+        "
+      />
+        <PriceForm
+          @newInit="
+            (value) => {
+              form.ingresso = value;
+            }
+          "
+          style="margin-bottom: 4px;"
+        />
+        <LimitForm
+          @newInit="
+            (value) => {
+              form.limitePessoa = value;
+            }
+          "
+        />
+      <div style="margin-top: 8px;">
+        <label class="formLabel">Local</label>
+        <s-input
+          v-model="form.endereco"
+          placeholder="Digite o endereço"
+          :prefix-icon="Location"
+        />
+      </div>
+      <div
+        :style="{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }"
+      >
+        <label class="formLabel">Gerar comunicado com IA</label>
+        <s-switch v-model="form.ia" />
+      </div>
+      <el-input
+        v-model="form.texto"
+        :rows="3"
+        :style="{ marginBottom: '22px' }"
+        type="textarea"
+        placeholder="Texto do comunicado"
+      />
+      <div>
+        <label class="formLabel">Enviar para</label>
+        <s-input v-model="form.destino" size="large" placeholder=""></s-input>
+      </div>
+      <div>
+        <el-upload
+          class="upload-demo"
+          drag
+          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+        >
+          <span>
+            <el-icon><Link /></el-icon>
+            Adicionar anexo
+          </span>
+          <template #tip>
+            <div class="el-upload__tip">max file size: 1MB</div>
+          </template>
+        </el-upload>
+      </div>
+    </s-card>
+    
+  </s-container>
+  <div class="send-button">
+    <s-button-menu
+      label="Enviar"
+      variant="primary-green"
+      @click="
+        () => {
+          console.log(form);
+        }
+      "
+    />
+</div>
+</div>
 </template>
 <style scoped>
-    .formLabel{
-        margin-bottom:12px !important
-    }
+.formLabel {
+  font-weight: bold;
+  font-size: 16px;
+  margin-bottom: 12px !important;
+}
+.send-button {
+    position: sticky;
+    bottom: 0;
+    padding: 16px;
+    background-color: white;
+}
+
+.page-container {
+    height: 100vh;
+}
 </style>
